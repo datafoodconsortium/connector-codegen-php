1ebdd769ab0a51d0bc66e378c9a4edc2b9feb6a7
Maxime Lecoq <maxime@lecoqlibre.fr>
2023-08-03 18:17:58 +0200

    Add unit tests and refactor

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # connector-codegen-php
 The connector makes things easier when integrating with DFC. This is the PHP version.
+
+wget -O phpunit.phar https://phar.phpunit.de/phpunit-10.phar
+curl -o phpunit.phar https://phar.phpunit.de/phpunit-10.phar
--- src/org/datafoodconsortium/connector/codegen/php/Generate.java
+++ src/org/datafoodconsortium/connector/codegen/php/Generate.java
@@ -116,7 +116,7 @@ public class Generate extends AbstractAcceleoGenerator {
      * 
      * @param args
      *            Arguments of the generation.
-     * @generated
+     * @generated NOT
      */
     public static void main(String[] args) {
         try {
